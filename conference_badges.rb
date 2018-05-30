d39885f917f6da7fb739d163300c329a4b81ec5f
Marie Cain <mariecain27@gmail.com>
2018-05-30 02:45:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 
 def assign_rooms(speakers)
   room_counter = 1
-  speakers.each do |name|
+  speakers.collect do |name|
   puts "Hello, #{name}! You'll be assigned to room #{room_counter}!"
   room_counter += 1
   end

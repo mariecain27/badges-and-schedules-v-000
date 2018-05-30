289b664f493e5c7a3758e82b243a380ed53f5611
Marie Cain <mariecain27@gmail.com>
2018-05-30 02:59:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,6 +24,7 @@ end
 def printer(speakers)
 # will output batch_badge_creator and assign_rooms
   speakers.each do |i|
-   puts badge_maker(i) binding.pry
+   puts badge_maker(i) 
+   binding.pry
  end
 end

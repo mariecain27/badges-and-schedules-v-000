30214843193f84e25a397ba5a568a204c5ce570a
Marie Cain <mariecain27@gmail.com>
2018-05-30 02:36:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,7 +7,6 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 rooms = [1, 2, 3, 4, 5, 6, 7]
 
 def batch_badge_creator(speakers)
-  #return an array of badge massages
   speakers.collect do |name|
     badge_maker(name)
   end
